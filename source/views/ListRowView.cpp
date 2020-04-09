e0ae2665b2ac53776e3eeb8597937321d612e77d
Nichole Mattera <697668+NicholeMattera@users.noreply.github.com>
2020-04-09 07:26:11 -0400

    Update copyright.

--- source/views/ListRowView.cpp
+++ source/views/ListRowView.cpp
@@ -1,5 +1,5 @@
 // Kosmos Updater
-// Copyright (C) 2019 Nichole Mattera
+// Copyright (C) 2020 Nichole Mattera
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
